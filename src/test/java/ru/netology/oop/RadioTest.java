ae0e80bf3caacbe9ab648b4168332d928d8fee5d
Марина Жукова <marinazhukova807@gmail.com>
2022-06-11 18:59:08 +0300

    add all volume tests

--- src/test/java/ru/netology/oop/RadioTest.java
+++ src/test/java/ru/netology/oop/RadioTest.java
@@ -1,7 +1,5 @@
 package ru.netology.oop;
-
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public class RadioTest {
@@ -15,5 +13,62 @@ public class RadioTest {
 
         assertEquals(expected, actual);
     }
+    @Test
+    void setMoreMaxVolumeTest() {
+        Radio rad = new Radio();
+        rad.setCurrentVolume(11);
+        int expected = 0;
+        int actual = rad.getCurrentVolume();
 
+        assertEquals(expected, actual);
+    }
+    @Test
+    void setLessMinVolumeTest() {
+        Radio rad = new Radio();
+        rad.setCurrentVolume(-11);
+        int expected = 0;
+        int actual = rad.getCurrentVolume();
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    void increaseVolumeTest() {
+        Radio rad = new Radio();
+        rad.setCurrentVolume(1);
+        rad.increaseVolume();
+        int expected = 2;
+        int actual = rad.getCurrentVolume();
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    void decreaseVolumeTest() {
+        Radio rad = new Radio();
+        rad.setCurrentVolume(1);
+        rad.decreaseVolume();
+        int expected = 0;
+        int actual = rad.getCurrentVolume();
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    void decreaseLessMinVolumeTest() {
+        Radio rad = new Radio();
+        rad.setCurrentVolume(0);
+        rad.decreaseVolume();
+        int expected = 0;
+        int actual = rad.getCurrentVolume();
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    void increaseMoreMaxVolumeTest() {
+        Radio rad = new Radio();
+        rad.setCurrentVolume(10);
+        rad.increaseVolume();
+        int expected = 10;
+        int actual = rad.getCurrentVolume();
+
+        assertEquals(expected, actual);
+    }
 }
