c54a5553180fbd7581c9763601f061d6ecdd21e2
Марина Жукова <marinazhukova807@gmail.com>
2022-06-11 18:04:04 +0300

    add new javaClass Radio.java

---
+++ src/main/java/ru/netology/oop/Radio.java
@@ -0,0 +1,18 @@
+package ru.netology.oop;
+
+public class Radio {
+    private int currentRadioStation;
+    private int currentVolume;
+
+    public void increaseVolume() {
+        if (currentVolume < 10) {
+            currentVolume = currentVolume + 1;
+        }
+    }
+
+    public void decreaseVolume() {
+        if (currentVolume > 0) {
+            currentVolume = currentVolume - 1;
+        }
+    }
+}
